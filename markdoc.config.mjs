a91cc868c3dd2ee84a8af3ae11ebe47f94e710d9
Will Cheng <will060710@gmail.com>
2026-07-13 01:25:42 +0800

    feat: highlight the code

---
+++ markdoc.config.mjs
@@ -0,0 +1,14 @@
+import { defineMarkdocConfig } from '@astrojs/markdoc/config';
+import shiki from '@astrojs/markdoc/shiki';
+
+export default defineMarkdocConfig({
+  extends: [
+    shiki({
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      wrap: false,
+    }),
+  ],
+});
